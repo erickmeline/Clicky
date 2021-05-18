54245a22264ce78a1b94d211eb6734d638942bf9
Erick Meline <erickmeline@gmail.com>
2021-05-17 19:53:12 -0700

    add some functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    count: 0
+    score: 0,
+    topscore: 0,
+    clicked: [],
+    headline: 'Click an image to begin!',
+    gameOver: false
   };
 
   randomDeck = () => {
@@ -20,18 +24,46 @@ class App extends React.Component {
   }
 
   handleClick = (e) => {
-    console.log(e.target);
+    let { clicked, score, topscore, gameOver } = this.state;
+    if (!gameOver) {
+      const id = e.target.getAttribute('data-id');
+      if (!clicked.includes(id)) {
+        clicked.push(id);
+        score++;
+        topscore = score > topscore ? score : topscore;
+        this.setState({clicked, score, topscore, headline: 'Nice! Now select another unique.'});
+      }
+      else {
+        topscore = score > topscore ? score : topscore;
+        this.setState({clicked, score, topscore, headline: 'You selected that one before.', gameOver: true});
+      }
+    }
   };
+
+  newGame = () => {
+    this.setState({
+      score: 0,
+      clicked: [],
+      headline: 'Click an image to begin!',
+      gameOver: false
+    });
+  }
 
   render() {
   const cards = this.randomDeck();
+  const { headline, score, topscore, gameOver } = this.state;
+  const classes = gameOver ? 'gameOver show' : 'gameOver hide';
   	return (
   		<div>
-  			<Navbar/>
+  			<Navbar score={score} topscore={topscore} headline={headline} gameOver={gameOver} />
         <main className="container">
           {
             cards.map(card => <Card image={card.image} id={card.id} onclick={(e) => {this.handleClick(e)}} key={card.id} />)
           }
+          <div className={classes}>
+            <h1>Game Over</h1>
+            <button onClick={() => {this.newGame()}}>Play Again?</button>
+          </div>
         </main>
   		</div>
   	);
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-function Navbar({...props}) {
+function Navbar({...props}) {console.log(props);
+    const color = props.gameOver ? '#900' : 'green';
     return (
         <nav className="navbar">
             <ul>
                 <li className="brand">
                     <a href="/">Clicky Game</a>
                 </li>
-                <li className="">
-                    Click an image to begin!
+                <li style={{color: `${color}`, background: 'rgba(0,0,0,0.4)'}}>
+                    {props.headline}
                 </li>
                 <li>
-                    Score: 0 | Top Score: 0
+                    Score: {props.score} | Top Score: {props.topscore}
                 </li>
             </ul>
         </nav>
